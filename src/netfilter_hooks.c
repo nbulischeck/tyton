59d0a754ef6740106c2ab4fea434163d5a95c809
nbulischeck <nbulischeck@users.noreply.github.com>
2018-12-03 10:52:15 -0500

    Updated netfilter hooks

--- src/netfilter_hooks.c
+++ src/netfilter_hooks.c
@@ -18,25 +18,25 @@ const char *mod_wl[] = {
 	"br_netfilter"
 };
 
-int in_module_whitelist(struct module *mod){
+int in_module_whitelist(const char *mod_name){
 	int i, name_len;
 
-	if (!mod->name)
+	if (!mod_name)
 		return -1;
 
-	name_len = strlen(mod->name);
+	name_len = strlen(mod_name);
 
 	for (i = 0; i < sizeof(mod_wl)/sizeof(mod_wl[0]); i++){
 		if (name_len != strlen(mod_wl[i]))
 			continue;
-		if (strncmp(mod->name, mod_wl[i], name_len))
+		if (strncmp(mod_name, mod_wl[i], name_len) == 0)
 			return 1;
 	}
 
 	return 0;
 }
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,16,0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,14,0)
 
 static void search_hooks(const struct nf_hook_entries *e){
 	int i;
@@ -46,14 +46,18 @@ static void search_hooks(const struct nf_hook_entries *e){
 
 	for (i = 0; i < e->num_hook_entries; i++){
 		addr = (unsigned long)e->hooks[i].hook;
+
 		mutex_lock(&module_mutex);
 		mod = get_module_from_addr(addr);
-		if (mod && !in_module_whitelist(mod)){
-				ALERT("Module [%s] controls a Netfilter hook.\n", mod->name);
+
+		if (mod && !in_module_whitelist(mod->name)){
+			ALERT("Module [%s] controls a Netfilter hook.\n", mod->name);
 		} else {
 			mod_name = find_hidden_module(addr);
-			ALERT("Module [%s] controls a Netfilter hook.\n", mod_name);
+			if (mod_name && !in_module_whitelist(mod_name))
+				ALERT("Module [%s] controls a Netfilter hook.\n", mod_name);
 		}
+
 		mutex_unlock(&module_mutex);
 	}
 }
@@ -118,62 +122,6 @@ int analyze_netfilter(void){
 	return 0;
 }
 
-#elif LINUX_VERSION_CODE >= KERNEL_VERSION(4,14,0)
-
-static void search_hooks(const struct nf_hook_entries *e){
-	int i;
-	const char *mod_name;
-	unsigned long addr;
-	struct module *mod;
-
-	for (i = 0; i < e->num_hook_entries; i++){
-		addr = (unsigned long)e->hooks[i].hook;
-		mutex_lock(&module_mutex);
-		mod = get_module_from_addr(addr);
-		if (mod && !in_module_whitelist(mod)){
-			ALERT("Module [%s] controls a Netfilter hook.\n", mod->name);
-		} else {
-			mod_name = find_hidden_module(addr);
-			ALERT("Module [%s] controls a Netfilter hook.\n", mod_name);
-		}
-		mutex_unlock(&module_mutex);
-	}
-}
-
-static struct nf_hook_entries __rcu **nf_hook_entry_head(struct net *net, int pf, unsigned int hooknum, struct net_device *dev){
-	if (pf != NFPROTO_NETDEV)
-		return net->nf.hooks[pf]+hooknum;
-	if (hooknum == NF_NETDEV_INGRESS){
-		if (dev && dev_net(dev) == net)
-			return &dev->nf_hooks_ingress;
-	}
-	return NULL;
-}
-
-int analyze_netfilter(void){
-	int i, j;
-	struct nf_hook_entries *p;
-	struct nf_hook_entries __rcu **pp;
-
-	INFO("Analyzing Netfilter Hooks\n");
-
-	for (i = 0; i < NFPROTO_NUMPROTO; i++){
-		for (j = 0; j < NF_MAX_HOOKS; j++){
-			pp = nf_hook_entry_head(&init_net, i, j, NULL);
-			if (!pp)
-				return -EINVAL;
-
-			p = nf_entry_dereference(*pp);
-			if (!p)
-				continue;
-
-			search_hooks(p);
-		}
-	}
-
-	return 0;
-}
-
 #else
 
 static void search_hooks(const struct list_head *hook_list){
@@ -186,11 +134,12 @@ static void search_hooks(const struct list_head *hook_list){
 		addr = (unsigned long)elem->hook;
 		mutex_lock(&module_mutex);
 		mod = get_module_from_addr(addr);
-		if (mod){
+		if (mod && !in_module_whitelist(mod->name)){
 			ALERT("Module [%s] controls a Netfilter hook.\n", mod->name);
 		} else {
 			mod_name = find_hidden_module(addr);
-			ALERT("Module [%s] controls a Netfilter hook.\n", mod_name);
+			if (mod_name && !in_module_whitelist(mod_name))
+				ALERT("Module [%s] controls a Netfilter hook.\n", mod_name);
 		}
 		mutex_unlock(&module_mutex);
 	}
@@ -208,6 +157,9 @@ static struct list_head *nf_find_hook_list(struct net *net, int pf, unsigned int
 
 int analyze_netfilter(void){
 	int i, j;
+
+	INFO("Analyzing Netfilter Hooks\n");
+
 	for (i = 0; i < NFPROTO_NUMPROTO; i++){
 		for (j = 0; j < NF_MAX_HOOKS; j++){
 			search_hooks(nf_find_hook_list(&init_net, i, j, NULL));
